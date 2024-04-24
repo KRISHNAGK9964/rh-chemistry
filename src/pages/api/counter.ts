f1cc38bf9bb61c1c8dd489c641864a7b7fbf8e33
Pranav Bhat <96832303+psbhatbvbcs@users.noreply.github.com>
2024-04-24 19:41:40 +0530

    [feat] - added live counter

--- src/pages/api/counter.ts
+++ src/pages/api/counter.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 
-const filePath = path.join(process.cwd(), "public", "counter.json");
+const filePath = "public/counter.json";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
